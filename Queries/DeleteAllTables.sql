65801c51a06632ec7d9b5aaf19c15f411f4f6d89
Gian Alfonso <93459007+Imanoodle13@users.noreply.github.com>
2025-06-19 23:18:26 -0500

    Query Additions

---
+++ Queries/DeleteAllTables.sql
@@ -0,0 +1,5 @@
+-- USE IF NECESSARY
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
---
+++ Queries/PostGIS_Activation.sql
@@ -0,0 +1,12 @@
+-- Assuming PostgreSQL (pgAdmin 4) is already installed,
+-- PostGIS installation:
+-- 1. Install PostGIS:
+-- 		Windows:	 Using Application Stack Builder (under PostgreSQL 16)
+--		macOS:		`brew install postgis`
+--		Linux:		`sudo apt install postgis postgresql-14-postgis-3`
+
+-- 2. Run the following queries:
+CREATE EXTENSION postgis;
+SELECT postgis_full_version();
+
+-- The `GEOGRAPHY(POINT,4326)` datatype should work.
