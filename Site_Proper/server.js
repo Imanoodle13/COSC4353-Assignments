4e5a4cd1ed7d4ba319fad162c4bd7c7f1da03ff2
Gian Alfonso <93459007+Imanoodle13@users.noreply.github.com>
2025-07-12 19:50:55 -0500

    Add event matcher Pug view and update server routes
    > Added a new Pug template for the event matcher page and registered new routes in server.js to render it.
    > Also included a markdown guide for converting HTML to Pug.
    > Minor HTML cleanup in the frontend event matcher page.

--- Site_Proper/server.js
+++ Site_Proper/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const pug = require('pug');
 
 const app = express();
-app.set('view engine', 'pug')
-app.set('views', './views')
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
 	res.render('index')
@@ -17,7 +17,10 @@ app.get('/homepage.html', function(req, res) {
 	res.render('homepage')
 });
 
+app.get(['/eventMatcher', '/eventmatcher.html'], (req, res) => {
+  res.render('eventMatcher');
+});
 
 app.listen(8080, () => {
-	console.log("listening")
+	console.log("listening at 8080...")
 });
