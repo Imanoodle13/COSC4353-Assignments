ad126e35ae16cbe7e65b6dbc3c814c439229ad7d
Kancherla, Rohan <rohan360k@gmail.com>
2025-08-01 21:16:52 -0500

    Turns out we may not need a username or password either
    The ssl reject unauthorized option might make it so that we don't have to worry about the rotating usernames and passwords from earlier. At least, that's if my testing is at all conclusive, which it may not be. I only tested SELECT queries, after all. Still, the original code is still there, just commented.

--- Site_Proper/database.js
+++ Site_Proper/database.js
@@ -2,18 +2,19 @@ require('dotenv').config();
 const session = require('express-session');
 const { Pool } = require('pg');
 const { app } = require('./server');
+const { url } = require('url')
 
 const pool = new Pool({
-	host:     process.env.PGHOST,
-	port:     Number(process.env.PGPORT || 5432),
-	user:     process.env.PGUSER,
-	password: process.env.PGPASSWORD,
-	database: process.env.PGDATABASE,
+	host: process.env.PGHOST,
+	port: Number(process.env.PGPORT || 5432),
+	// user:     process.env.PGUSER,
+	// password: process.env.PGPASSWORD,
+	// database: process.env.PGDATABASE,
 	ssl: { rejectUnauthorized: false },// set to `true` in prod with a valid CA
 	enableChannelBinding: true  // Only meaningful if Postgres offers SCRAM‑SHA‑256‑PLUS
 });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
-  connect: () => pool.connect(),
+	query: (text, params) => pool.query(text, params),
+	connect: () => pool.connect(),
 };
